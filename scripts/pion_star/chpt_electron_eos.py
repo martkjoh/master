4334cd85e3e3035e32257a416d536baeb3a61636
Martin Johnsrud <martin.johnsrud@gmail.com>
2022-03-30 12:06:10 +0200

    fixed charge neutral plots

--- scripts/pion_star/chpt_electron_eos.py
+++ scripts/pion_star/chpt_electron_eos.py
@@ -15,17 +15,23 @@ plt.rc("axes", grid=True)
 plt.rc("grid", linestyle="--", alpha=1)
 
 
+# x = mu_\ell
 xf = lambda x: sqrt(x**2 - 1)
-ul = lambda x: (2*xf(x)**3 + xf(x)) * sqrt(1 + xf(x)**2) - arcsinh(xf(x)) 
-pl = lambda x: 1 / 3 * ((2*xf(x)**3 - 3*xf(x)) * sqrt(1 + xf(x)**2) + 3*arcsinh(xf(x)))
+pl = lambda x: 1 / 3 * ((2*xf(x)**3 - 3*xf(x)) * x + 3*arcsinh(xf(x))) 
+ul = lambda x: (2*xf(x)**3 + xf(x)) * x - arcsinh(xf(x))
 
-ul = lambda x: (2*x**3 + x) * sqrt(1 + x**2) - arcsinh(x) 
-pl = lambda x: 1 / 3 * ((2*x**3 - 3*x) * sqrt(1 + x**2) + 3*arcsinh(x))
+# pl = lambda x: 1 / 3 * ((2*x**3 - 3*x) * sqrt(1 + x**2) + 3*arcsinh(x))
+# ul = lambda x: (2*x**3 + x) * sqrt(1 + x**2) - arcsinh(x) 
 
+# mu_\ell = sqrt(1+x) = 1+x/2
+pl_lim0 = lambda x: 8/15*x**(5)
+ul_lim0 = lambda x: 8/3*x**3
 
+# x = mu_I
 p_pi = lambda  x: 1/2 * (x - 1/x)**2
 u_pi = lambda x: 1/2 * (2 + x**2 - 3/x**2)
 
+# mu_I = sqrt(1+x) = 1 + x/2
 p_pi_lim = lambda x: 1/2*x**2
 u_pi_lim = lambda x: 2*x
 
@@ -42,13 +48,17 @@ def get_l(m_l):
 
     # x = mu_I / m_pi, y = mu_l / m_l
     eq_l = lambda x, y : A * (y**2 - 1)**(3/2) - 1*x*(1 - 1/x**4)
-    f_l = lambda x: sqrt(1 + 1/A**(2/3) * (sqrt(1+x**2) * (1-1/(1+x**2)**2))**(2/3)  )
+    # mu_\ell(mu_I)
+    f_l = lambda x: sqrt(1 + 1/A**(2/3) * (x * (1-1/x**4))**(2/3))
 
-    u = lambda x: u_pi(sqrt(1+x**2)) + ul0/u0 * ul(sqrt(f_l(x)**2 -1))
-    p = lambda x: p_pi(sqrt(1+x**2)) + ul0/u0 * pl(sqrt(f_l(x)**2 -1))
+    # mu_i = sqrt(1+x**2)
+    u = lambda x: u_pi(x) + ul0/u0 * ul(f_l(x))
+    p = lambda x: p_pi(x) + ul0/u0 * pl(f_l(x))
 
     K1 = 8/15*(2/A)**(5/3)
     K2 = 16/3/A
+    # mu_I = sqrt(1+x) = 1 + x/2
+    # in units of u0
     pl_lim = lambda x: K1 * x**(5/3)
     ul_lim = lambda x: K2 * x
 
@@ -58,11 +68,11 @@ def get_l(m_l):
 N = 400
 
 def plot_mus(m_l=m_e, name="e"):
-    x = np.linspace(0, 2, N)
+    x = 1+np.linspace(0, 2, N)
     fig, ax = plt.subplots(figsize=(8, 6))
 
     f_l, p, u, pl_lim, ul_lim, A, ul0 = get_l(m_l)
-    ax.plot(sqrt(1+x**2), f_l(x), "k")
+    ax.plot(x, f_l(x), "k")
     ax.set_xlabel("$\\mu_I/m_\\pi$")
     ax.set_ylabel("$\mu_"+name+"/m_"+name+"$")
 
@@ -74,15 +84,16 @@ def plot_mus(m_l=m_e, name="e"):
 
 # limit
 def plot_mus_lim(m_l=m_e, name="e"):
-    x = np.logspace(-6, 2, N)
+    x = 1+np.logspace(-20, 4, N)
     fig, ax = plt.subplots(figsize=(12, 8))
 
     f_l, p, u, pl_lim, ul_lim, A, ul0 = get_l(m_l)
-    ax.plot(sqrt(1+x**2)-1, f_l(x)-1)
+    ax.plot(x-1, f_l(x)-1, lw=5, alpha=0.6)
     ax.set_xlabel("$\\mu_I/m_\\pi$")
     ax.set_ylabel("$\mu_\\ell/m_\\ell$")
 
-    ax.plot(x**2/2, (2*x**2/A)**(2/3)/2, "r-.")
+    eps = (x-1)
+    ax.plot(eps/2, (2*eps/A)**(2/3)/2, "k--", lw=2)
     ax.set_xscale("log")
     ax.set_yscale("log")
 
@@ -93,18 +104,17 @@ def plot_lepton(name="\\ell"):
     fig, ax = plt.subplots(figsize=(8, 5))
 
     f_l, p, u, pl_lim, ul_lim, A, ul0 = get_l(100)
-    x = np.linspace(0, 0.5, N)
-    y = sqrt(f_l(x)**2 - 1)
-    ax.plot(pl(y), ul(y), label="$u_{\\ell, \mathrm{nr}}(p)$")
+    x = 1+np.linspace(0, 0.2, N)
+    ax.plot(pl(x), ul(x), label="$u_{\\ell, \mathrm{nr}}(p)$")
 
-    x = (sqrt(1+ x**2)-1)*2
-    ax.plot(pl_lim(x), ul_lim(x), "k--", label="$u_{\\ell, \\mathrm{nr}}$")
+    y = sqrt(x**2-1)
+    ax.plot(pl_lim0(y), ul_lim0(y), "k--", label="$u_{\\ell, \\mathrm{nr}}$")
 
     # assert (y[0]==0)
     tit = "$\\frac{\mu_"+name+"}{m_"+name+"}  \in [1,%.2f], \\quad" %(1+x[-1])+"$"
     ax.set_title(tit)
-    ax.set_xlabel("$p/u_{\\ell,\mathrm{nr}}$")
-    ax.set_ylabel("$u/u_{\\ell,\mathrm{nr}}$")
+    ax.set_xlabel("$p/u_{\\ell,0}$")
+    ax.set_ylabel("$u/u_{\\ell,0}$")
 
     ax.legend()
     fig.savefig("figurer/charge_neutrality/eos_lepton.pdf", bbox_inches="tight")
@@ -115,10 +125,11 @@ def plt_tot(m_l=m_e, name="e"):
     fig, ax = plt.subplots(figsize=(6, 6))
 
     f_l, p, u, pl_lim, ul_lim, A, ul0 = get_l(m_l)
-    x = np.logspace(-5, 2, N, dtype=np.longdouble())
+    x = 1+np.logspace(-11, 2, N, dtype=np.longdouble())
     ax.plot(p(x), u(x), label="$u(p)$")
 
     a = ul0/u0
+    x = (x-1)*2
     p = a*pl_lim(x)
     ax.plot(p, u_pi_lim(x) + a*ul_lim(x), "k--", label="$p\propto u^{5/3}$")
 
@@ -165,8 +176,12 @@ def plot_all(r, name="nr"):
     x = 1 + np.logspace(*r, N-1)
     x = np.concatenate([[1], x])
 
-    ue, pe = load_eos_list("e")
-    umu, pmu = load_eos_list("mu")
+    f_l, p, u, pl_lim, ul_lim, A, ul0 = get_l(m_e)
+    pe = p(x)
+    ue = u(x)
+    f_l, p, u, pl_lim, ul_lim, A, ul0 = get_l(m_mu)
+    pmu = p(x)
+    umu = u(x)
 
     ax.plot(p_pi(x), u_pi(x), label="$u_\\pi(p)$")
     ax.plot(pe, ue, "k--", label="$u_e(p)+u_\\pi(p)$")
@@ -185,22 +200,18 @@ def plot_all(r, name="nr"):
 # plot_mus_lim(m_l=m_e, name="e")
 
 # plot_lepton()
+ 
+# plt_tot(m_l=m_e, name="e")
+# plt_tot(m_l=m_mu, name="\\mu")
 
-plt_tot(m_l=m_e, name="e")
-plt_tot(m_l=m_mu, name="\\mu")
-
-# plot_all()
-
-# r = (-13, 3)
+# r = (-13, 2)
 # x = 1 + np.logspace(*r, N-1)
 # gen_eos_list(x, m_e, name="e")
+# gen_eos_list(x, m_mu, name="mu")
 
-
-# r = (-12, 2)
-# xm1 = np.logspace(*r, N-1, dtype=np.longdouble())
-# xm1 = np.concatenate([[0], xm1])
-# gen_eos_list(xm1, m_mu, name="mu")
-
-# plot_all(r, name="ur")
+r = (-5,-2)
+plot_all(r, name="nr")
+r = (-2, 1)
+plot_all(r, name="ur")
 
 
